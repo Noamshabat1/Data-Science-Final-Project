8ff81602103f2d8bed899519d18cf4d3221e7b76
Dan8Oren <dano@wix.com>
2025-07-31 20:08:48 +0300

    main python file to run all code, and created new requirements text file

---
+++ main.py
@@ -0,0 +1,22 @@
+from data_processing_runner import main as data_processing_main
+from model.build_data import load_and_merge_data
+from model.models import main as model_main
+
+
+# This script orchestrates the data preprocessing, model data building, and model training/evaluation.
+def main():
+    print(" >>> Starting date preprocessing...")
+    data_processing_main()
+
+    print(" >>> Starting model data building...")
+    df_final = load_and_merge_data()
+    print("Sample of the final dataset:")
+    print(df_final.head())
+
+    print(" >>> Starting model training and evaluation...") 
+    model_main()
+
+if __name__ == "__main__":
+    main()
+
+
